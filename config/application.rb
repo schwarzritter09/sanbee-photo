fabe486eea36893176ca1223dfbf33d7f7962782
unknown <le.cadeau.de.un.ange0911@gmail.com>
2015-10-07 18:17:10 +0900

    change assets config

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module SanbeePhoto
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.assets.compile = true
+    config.assets.digest = true
     config.serve_static_files = true
   end
 end
