9a984af8fca2ce9adf19143dc3353c370eb860fb
schwarzritter09 <le.cadeau.de.un.ange0911@gmail.com>
2015-12-03 10:50:38 +0900

    remove turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.turbolinks
-//= require turbolinks
 //= require_tree .
 //= require jquery_nested_form
 //= require jquery.infinitescroll
